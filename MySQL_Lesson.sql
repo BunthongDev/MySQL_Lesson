b8ecbb6cff3e05cf8631a8b048a454f5ea7a754d
BunthongRorn <bunthongran579@gmail.com>
2024-09-01 16:11:20 +0700

    Update

--- MySQL_Lesson.sql
+++ MySQL_Lesson.sql
@@ -25,3 +25,16 @@ SELECT customer_id, shipper, freight FROM orders WHERE freight >= 100;
 -- Returns all the customers in the OR , USA
 SELECT id, company_name, region, country FROM customers
 WHERE country = "USA" AND region = 'OR';
+
+-- Find the order_date BETWEEN '2019-01-01' AND '2019-12-31'
+SELECT id, order_date, shipper, freight FROM orders
+WHERE order_date BETWEEN '2019-01-01' AND '2019-12-31';
+
+-- Filtering with SQL OR operator
+SELECT id, country FROM customers 
+WHERE country = 'USA' OR country = 'Germany';
+
+-- SQL ORDER BY keyword Syntax Ascending (ASC) and Descending (DESC)
+SELECT id, company_name, region, country FROM customers 
+ORDER BY country ASC;
+
